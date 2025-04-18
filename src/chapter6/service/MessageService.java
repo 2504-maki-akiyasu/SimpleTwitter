8c17479f570ed3fb6dcbfd41af5da16fe9fe5913
maki_akiyasu <maki.akiyasu@alhinc.jp>
2025-04-18 16:47:13 +0900

    first commit

---
+++ src/chapter6/service/MessageService.java
@@ -0,0 +1,54 @@
+package chapter6.service;
+
+import static chapter6.utils.CloseableUtil.*;
+import static chapter6.utils.DBUtil.*;
+
+import java.sql.Connection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import chapter6.beans.Message;
+import chapter6.dao.MessageDao;
+import chapter6.logging.InitApplication;
+
+public class MessageService {
+
+
+    /**
+    * ロガーインスタンスの生成
+    */
+    Logger log = Logger.getLogger("twitter");
+
+    /**
+    * デフォルトコンストラクタ
+    * アプリケーションの初期化を実施する。
+    */
+    public MessageService() {
+        InitApplication application = InitApplication.getInstance();
+        application.init();
+
+    }
+
+    public void insert(Message message) {
+
+	  log.info(new Object(){}.getClass().getEnclosingClass().getName() + 
+        " : " + new Object(){}.getClass().getEnclosingMethod().getName());
+
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            new MessageDao().insert(connection, message);
+            commit(connection);
+        } catch (RuntimeException e) {
+            rollback(connection);
+		log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+            throw e;
+        } catch (Error e) {
+            rollback(connection);
+		log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+            throw e;
+        } finally {
+            close(connection);
+        }
+    }
+}
